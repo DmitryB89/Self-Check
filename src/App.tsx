6b9b5adeec39014285ea1facf01cd2daf54d16d6
Dmitry <d.baydarov@yandex.ru>
2022-10-17 00:02:56 +0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HW1 from "./s2-homeworks/hw01/HW1";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <HW1/>
+        </div>
+    );
 }
 
 export default App;
+//  "homepage": "http://IgnatZakalinsky.github.io/hws2",
